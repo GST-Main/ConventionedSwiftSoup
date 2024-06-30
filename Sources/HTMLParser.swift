b402b00f80f12d4968550036835e3b44e6eaeaee
GST <gst.soft.65536@gmail.com>
2024-06-30 15:48:46 +0900

    Apply Inheritence of `MarkupParser` to `HTMLParser`

--- Sources/HTMLParser.swift
+++ Sources/HTMLParser.swift
@@ -11,38 +11,10 @@ import Foundation
 /// Parses HTML into a ``Document``.
 ///
 /// Generally, static method ``parse(_:baseURI:)-swift.type.method`` is mostly recommended.
-public class HTMLParser {
-	private static let DEFAULT_MAX_ERRORS: Int = 0 // by default, error tracking is disabled.
-
-	public var treeBuilder: TreeBuilder
-	public var maxErrors: Int = DEFAULT_MAX_ERRORS
-	public private(set) var errors: ParseErrorList = ParseErrorList(16, 16)
-    public var isTrackErrors: Bool { maxErrors > 0 }
-	public var settings: ParseSettings
-
-    /// Create a new ``HTMLParser`` using the specified ``TreeBuilder``
-    /// - Parameters:
-    ///     - treeBuilder: A ``TreeBuilder`` object to use to parse input into ``Document``s.
-	init(_ treeBuilder: TreeBuilder) {
-		self.treeBuilder = treeBuilder
-        self.settings = treeBuilder.defaultSettings()
-	}
-
-    /// Parse HTML into a ``Document``.
-    ///
-    /// - Parameters:
-    ///     - html: HTML string to parse.
-    ///     - baseURI: Base URI of document for resolving resolving relative URLs. To see how it can be used, see ``Node/absoluteURLPath(ofAttribute:)``.
-    /// - Returns: Parsed ``Document`` object.
-    ///
-    /// You can track parse errors whereas static method ``parse(_:baseURI:)-swift.type.method`` can't.
-    ///
-    /// ## Throws:
-    /// * `SwiftSoupError.failedToParseHTML`` if parsing is failed.
-	public func parse(_ html: String, baseURI: String) throws -> Document {
-		errors = isTrackErrors ? ParseErrorList.tracking(maxErrors) : ParseErrorList.noTracking()
-		return try treeBuilder.parse(html, baseURI, errors, settings)
-	}
+public class HTMLParser: MarkupParser {
+    public init() {
+        super.init(HtmlTreeBuilder())
+    }
 
 	// MARK: Static Methods
     /// Parse HTML into a ``Document``.
